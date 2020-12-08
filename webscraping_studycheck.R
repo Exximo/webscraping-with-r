8f254d1710ef05e90edde4092011142bd0aa263a
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2020-12-08 21:45:09 +0100

    Add files via upload

---
+++ webscraping_studycheck.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+library(rvest)
+# Seite definieren
+hdm_studycheck <- read_html("https://www.studycheck.de/studium/online-medien/hdm-stuttgart-15774")
+
+# Bewertungen mit CSS-Selektoren auswählen
+bewertungen_roh <- hdm_studycheck %>% 
+        html_nodes(".item-text") %>%
+        html_text() %>%
+        as.character()
+
+# Daten in einen Dataframe umwandeln
+bewertungen_df <- tibble(bewertungen_roh)
+
+library(tidytext)
+# Unnest Tokens
+bewertungen <- 
+  bewertungen_df %>%
+  unnest_tokens(word, bewertungen_roh)
+
+library(tm)
+# Stopwörter entfernen 
+stopwords <- tibble( word = stopwords('german'))
+
+bewertungen <- 
+  bewertungen %>%
+  anti_join(stopwords)
+
+# Wörter zählen
+count(bewertungen, word, sort = TRUE)
+
+# Darstellung erzeugen
+bewertungen %>%
+  count(word, sort = TRUE) %>%
+  filter(n >= 2) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  coord_flip() 
+
+library(wordcloud)
+# Wordcloud erzeugen 
+bewertungen %>%
+  count(word, sort=TRUE) %>%
+  with(wordcloud(word, n, max.words = 2))
